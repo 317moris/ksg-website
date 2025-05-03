00fad35e46ca87b669e2380c5a591d19dca19644
317moris <mori.souta.fu@st.spec.ed.jp>
2025-05-03 17:04:35 +0900

    最新状態を取得できないのを修正

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,13 +4,28 @@ import type { Post, Posts } from "@/interfaces/post";
 import { cmsClient } from "./microcms";
 
 export async function getPostBySlug(slug: string) {
-	const post: Post = await cmsClient.get({ endpoint: "news", contentId: slug });
+	const post: Post = await cmsClient.get({
+		endpoint: "news",
+		customRequestInit: {
+			next: {
+				revalidate: 60,
+			},
+		},
+		contentId: slug,
+	});
 
 	return post;
 }
 
 export async function getAllPosts() {
-	const posts: Posts = await cmsClient.get({ endpoint: "news" });
+	const posts: Posts = await cmsClient.get({
+		endpoint: "news",
+		customRequestInit: {
+			next: {
+				revalidate: 60,
+			},
+		},
+	});
 
 	return posts;
 }
