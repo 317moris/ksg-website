7bd9134f298567bb1d9e126421d293c5f97c6ea9
317moris <mori.souta.fu@st.spec.ed.jp>
2025-05-26 20:20:58 +0900

    レイアウト調整

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -2,40 +2,42 @@ import { getAllPosts } from "@/lib/api";
 import {
 	Card,
 	ClientOnly,
-	For,
+	HStack,
 	LinkBox,
 	LinkOverlay,
 	Text,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { FaUpload } from "react-icons/fa6";
 import { DateFormatter } from "./date-formatter";
 
 export default async function Posts() {
 	const posts = await getAllPosts();
 
-	return (
-		<For each={posts.contents}>
-			{(post) => (
-				<LinkBox key={post.id}>
-					<Card.Root maxH="xs" overflowY="clip">
-						<Card.Body>
-							<ClientOnly>
-								<Text color="fg.muted" fontFamily="mono">
-									<DateFormatter date={post.createdAt} />
-								</Text>
-							</ClientOnly>
-							<Card.Title>
-								<LinkOverlay asChild>
-									<NextLink href={`/news/${post.id}`}>{post.title}</NextLink>
-								</LinkOverlay>
-							</Card.Title>
-							{post.subtitle ? (
-								<Card.Description>{post.subtitle}</Card.Description>
-							) : null}
-						</Card.Body>
-					</Card.Root>
-				</LinkBox>
-			)}
-		</For>
-	);
+	return posts.contents.map((post) => (
+		<LinkBox key={post.id}>
+			<Card.Root h="full" size="sm">
+				<Card.Body>
+					<Card.Title asChild>
+						<LinkOverlay asChild>
+							<NextLink href={`/news/${post.id}`}>{post.title}</NextLink>
+						</LinkOverlay>
+					</Card.Title>
+					{post.subtitle ? (
+						<Card.Description>{post.subtitle}</Card.Description>
+					) : null}
+				</Card.Body>
+				<Card.Footer justifyContent="end">
+					<ClientOnly>
+						<HStack color="fg.muted" fontFamily="mono">
+							<FaUpload />
+							<Text>
+								<DateFormatter date={post.createdAt} />
+							</Text>
+						</HStack>
+					</ClientOnly>
+				</Card.Footer>
+			</Card.Root>
+		</LinkBox>
+	));
 }
