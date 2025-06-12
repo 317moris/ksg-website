dea1a33f58328f04159dde68a7c723efdc18911d
317moris <mori.souta.fu@st.spec.ed.jp>
2025-06-12 11:10:21 +0900

    設定したはずのものが消えていた

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
 	experimental: {
 		optimizePackageImports: ["@chakra-ui/react"],
 	},
+	images: {
+		remotePatterns: [
+			new URL(
+				"https://images.microcms-assets.io/assets/47f3328d673f4ec79dbc34797a3220a2/**",
+			),
+		],
+	},
 };
 
 export default nextConfig;
